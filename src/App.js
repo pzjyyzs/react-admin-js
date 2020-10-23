adf30bdc4038f1f4592b9af9d3770dd7d0f46bf4
pzjyyzs <463669395@qq.com>
2020-10-23 23:02:55 +0800

    路由引入

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import Home from './views/Home';
+import About from './views/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <HashRouter>
+      <Switch>
+        <Route component={Home} exact path='/'>home</Route>
+        <Route component={About} path='/about'>about</Route>
+      </Switch>
+    </HashRouter>
+  )
 }
 
 export default App;
---
+++ src/views/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+
+    render() {
+        return <div>about</div>
+    }
+}
+
+export default About
